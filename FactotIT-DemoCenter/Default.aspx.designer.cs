0f51dffa5ebeb7713ccd74d7c791db3f1a2a7c2e
FACTOR IT DEVELOPER <enrique.gonzalez@factorit.com>
2023-01-17 11:24:03 -0500

    Inicio main
    Cambio default, prueba GIT
    
    Agregar archivos de proyecto.

---
+++ FactotIT-DemoCenter/Default.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace FactotIT_DemoCenter
+{
+
+
+    public partial class _Default
+    {
+    }
+}
---
+++ FactotIT-DemoCenter/main/factorit-demo-center.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace FactotIT_DemoCenter.main
+{
+
+
+    public partial class factorit_demo_center
+    {
+    }
+}
