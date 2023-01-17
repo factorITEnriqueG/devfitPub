fdd072c93bd227f330554fd3de564e04c913db53
FACTOR IT DEVELOPER <enrique.gonzalez@factorit.com>
2023-01-17 15:23:07 -0500

    Merge

---
+++ FactotIT-DemoCenter/main/factorit-demo-center.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FactotIT_DemoCenter.main
+{
+    public partial class factorit_demo_center : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
